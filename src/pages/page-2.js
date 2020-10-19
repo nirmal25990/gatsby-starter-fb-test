badb98d74d31bf46371faa47af47b5bf0cb46049
Nirmal Patel <nirmal25990@gmail.com>
2020-10-19 18:06:35 +0530

    check proxy

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,7 +10,7 @@ const SecondPage = () => (
           description="Poor indoor air quality affects people's ability to concentrate and think clearly." />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/quiz">Go back to the homepage</Link>
+    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
