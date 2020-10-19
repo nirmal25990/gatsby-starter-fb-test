95f2e713fe3c6633844382059671a84aecd8ea12
Nirmal Patel <nirmal25990@gmail.com>
2020-10-19 18:24:24 +0530

    test fb share

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,10 +6,10 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Cognitive Effects of Poor Indoor Air Quality"
+    <SEO title=" Test Cognitive Effects of Poor Indoor Air Quality"
           description="Poor indoor air quality affects people's ability to concentrate and think clearly." />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to proxy page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
